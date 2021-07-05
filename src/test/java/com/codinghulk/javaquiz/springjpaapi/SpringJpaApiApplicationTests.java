866f4689b196c30ffaca844ab7679fe662044e74
falkowa42 <falkowa42@gmail.com>
2021-07-05 08:32:31 -0400

    initial commit

---
+++ src/test/java/com/codinghulk/javaquiz/springjpaapi/SpringJpaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codinghulk.javaquiz.springjpaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
